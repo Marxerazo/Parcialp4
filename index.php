7bb07c47dc187bb689c73a39aa20b67e96b390a3
Marxerazo <marx050615@gmail.com>
2017-02-10 12:59:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+         session_start();
+        include 'conexion.php';
+        
+        function verificar_login($usuario,$clave,&$result) {
+        $sql = "SELECT * FROM user WHERE usuario = '$usuario' and clave = '$clave'";
+        $rec = mysql_query($sql);
+        $count = 0;
+    while($row = mysql_fetch_object($rec))
+    {
+        $count++;
+        $result = $row;
+    }
+    if($count == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+if(!isset($_SESSION['idusu']))
+{
+    if(isset($_POST['login']))
+    {
+        if(verificar_login($_POST['usuario'],$_POST['clave'],$result) == 1)
+        {
+            $_SESSION['idusu'] = $result->idcodigo;
+            header("location:index.php");
+        }
+        else
+        {
+            echo '<div class="error">Su usuario es incorrecto, intente nuevamente.</div>';
+        }
+    }
+?>
+
+<style type="text/css">
+*{
+	font-size: 14px;
+}
+form.login {
+    background: none repeat scroll 0 0 #F1F1F1;
+    border: 1px solid #DDDDDD;
+    font-family: sans-serif;
+    margin: 0 auto;
+    padding: 20px;
+    width: 278px;
+}
+form.login div {
+    margin-bottom: 15px;
+    overflow: hidden;
+}
+form.login div label {
+    display: block;
+    float: left;
+    line-height: 25px;
+}
+form.login div input[type="text"], form.login div input[type="password"] {
+    border: 1px solid #DCDCDC;
+    float: right;
+    padding: 4px;
+}
+form.login div input[type="submit"] {
+    background: none repeat scroll 0 0 #DEDEDE;
+    border: 1px solid #C6C6C6;
+    float: right;
+    font-weight: bold;
+    padding: 4px 20px;
+}
+.error{
+    color: red;
+    font-weight: bold;
+    margin: 10px;
+    text-align: center;
+}
+</style>
+
+<form action="" method="post" class="login">
+	<div><label>usuario</label><input name="usuario" type="text" ></div>
+	<div><label>clave</label><input name="clave" type="password"></div>
+	<div><input name="login" type="submit" value="login"></div>
+</form>
+<?php
+} else {
+	echo 'USUARIO CORRECTO'.'<br>';
+        echo '<br>';
+        
+	echo '<a href="main/index1.php">lista de productos </a>';
+  
+}
+?>
+        
+    </body>
+</html>
